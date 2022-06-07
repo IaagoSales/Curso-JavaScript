c694feaf48456464ec83599655d360d0d200126f
Iago Sales <99757969+IagoSalesP@users.noreply.github.com>
2022-06-07 04:10:11 -0300

    Iago_07062022_01

---
+++ Aula11/index.js
@@ -0,0 +1,37 @@
+/**
+ *  Operadores Aritméticos:
+ * 
+ *      +  Adição | Concatenação 
+ *      - Subtração 
+ *      / Divisão 
+ *      * Multiplicação 
+ *      ** Potencia
+ *      % Retorna o resto da divisão
+ *      ++ Operador de incremento 
+ *      -- Operador de decremento 
+ *      
+ *          NaN = Not a Number 
+ *      
+ *      parseInt = Conversão para inteiro
+ *      parseFloat = Conversão para decimais
+ *      
+ */
+
+
+const num1 = 2;
+const num2 = 10;
+console.log(num1 ** num2);
+
+let contador = 1;
+contador++;
+console.log(contador);
+
+contador = contador + 2;  // ou contador += 2; // 
+console.log(contador);
+
+const num3 = 10;
+const num4 = parseInt('5.2');
+
+
+
+
