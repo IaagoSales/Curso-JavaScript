5bb13ab3a24a2a700e98bb95ef29f4647e79e716
Iago Sales <99757969+IagoSalesP@users.noreply.github.com>
2022-06-08 04:02:11 -0300

    Iago_08062022_01

---
+++ Aula12/js/script.js
@@ -0,0 +1,19 @@
+/*
+
+alert('Olá Bem-Vindo!');
+
+confirm('Deseja mesmo apagar');
+
+prompt('Digite o seu Nome: ');
+
+*/
+
+let num1 = prompt('Digite um número: ');
+let num2 = prompt('Digite um número: ');
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+const resultado = (num1 + num2);
+
+alert('O resultado é: ' + resultado);
