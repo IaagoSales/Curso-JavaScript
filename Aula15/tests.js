2245bcb37fd8fa8cf7a50133fad89c6742bc04ad
Iago Sales <iagosales123l@hotmail.com>
2024-06-27 14:48:53 -0300

    2024-06-27

---
+++ Aula15/tests.js
@@ -0,0 +1,27 @@
+// IEEE 754-2008
+let num1 = 0.7;  // number
+let num2 = 0.1;  // number
+
+num1 += num2; // isso é igual a num1 = num1 + num2;
+num1 += num2; // 0.9
+num1 += num2; // 1.0
+
+
+
+console.log(num1);
+console.log(Number.isInteger(1.00))
+
+// console.log(num1.toString() + num2);  // toString: Converte um valor "temporariamente" para string;
+// num1 = num1.toString // Maneira para alterar para String definitivamente.
+// console.log(typeof num1);
+// console.log(num1.toString(2));  // toString(2)) Transforma em binário
+// console.log(num1.toFixed(2));  // toFixed(x)) Fixa o número de casas decimais desejados
+// console.log(Number.isInteger(num1)); // isInteger: Retornar se o número é inteiro ou não
+// let temp = num1 * '5'; // Não faça contas com Strings
+// console.log(Number.isNaN(temp)); // isNaN: Retorna verdadeiro se a conta for inválida.
+
+
+
+
+
+
---
+++ Aula16/index.js
@@ -0,0 +1,22 @@
+// Objeto Math //
+
+let num1 = 9;
+// let num2 = Math.floor(num1);  // Math.floor: Função de arredondar os números para baixo
+// let num2 = Math.ceil(num1); // Math.ceil: Função de arredondar os números para cima
+// let num2 = Math.round(num1); // Math.round: Função arredonda para o mais próximo 1.49 = 1.0 e 1.50 = 2
+// console.log(Math.max(1,2,3,4,5,6-1,-2)); // Math.max Dentre os números listado ele só retorna o maior número
+// console.log(Math.min(1,2,3,4,5,6-1,-2)); // Math.min Dentre os números listados ele só retorna o menor número
+// console.log(Math.random()); // Math.random Gera um número aleatório entre 0 e 1
+// console.log(Math.pow(2, 10)); // Math.pow Ele pega um número x e o eleva a um número y
+console.log(num1 ** 0.5);
+
+// No JS é possível dividir por 0
+
+
+
+
+
+
+
+
+
