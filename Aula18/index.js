b89fefaaf6fdd074f94994546983b6ceff64b341
Iago Sales <iagosales123l@hotmail.com>
2024-06-28 12:43:26 -0300

    2024-06-28
    Aula 17, 18 e 19

---
+++ Aula18/index.js
@@ -0,0 +1,39 @@
+// Arrays básicos \\
+
+//                 0        1        2       \\
+const alunos =  ['Luiz', 'Maria', 'João'];
+
+
+console.log(typeof alunos); 
+console.log(alunos instanceof Array); // instanceof Array - Forma para ter certeza que estou trabalhando com Array. \\
+console.log(alunos);
+
+
+
+// alunos.push('Debora');
+// alunos.push('Gabrielle');
+// console.log(alunos.slice(0, 3))  // Slice - Serve para fatiar, pegar até determinado array.
+
+// delete alunos[1]; // delete - Deleta o arrya, porem o indíce permanece \\
+
+// const rem1 = alunos.pop(); // pop - Retira o ultimo array, os indíces são alterados \\
+// const rem2 = alunos.shift(); // shitf - Retira o primeiro array, os indíces são alterados \\
+// console.log(rem2);
+// console.log(rem1);
+
+
+// alunos.unshift('Luiza'); // unshift adiciona o array no começo \\ 
+// alunos.unshift('Fábio'); // Fábio vai vir antes de Luiza \\
+
+// alunos.push('Otávio'); // push, adicionar o array no final de forma mais eficaz \\
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza'; // Forma de adicionar um array no final. \\
+// console.log(alunos.length);
+
+// alunos[3] = 'Eduardo';  // Editar array, serve tanto para adicionar quanto pra substituir \\
+// console.log(alunos);
+
+// console.log(alunos);    
+
+
---
+++ Aula19/index.js
@@ -0,0 +1,25 @@
+// function soma(x = 1, y = 1){
+//    const resultado = x + y;
+//    return resultado; // Após a o return, nada mais será executado. \\
+//    
+//}
+
+//console.log(soma());
+//console.log(soma(2, 7)); 
+
+// const raiz  = function(n){
+//    return n ** 0.5;
+//};
+
+// console.log(raiz(9));
+// console.log(raiz(81));
+
+
+// ElonFunction \\
+// Maneira "simplificada" de fazer uma função.
+
+// const raiz = n => n ** 0.5;
+
+// console.log(raiz(9));
+// console.log(raiz(25));
+// console.log(raiz(81));
