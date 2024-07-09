cc726cba6c9282e6ca3f120f260eb7a7711a444e
Iago Sales <iagosales123l@hotmail.com>
2024-07-09 12:00:53 -0300

    2024-07-09

---
+++ Aula27/index.js
@@ -0,0 +1,11 @@
+// ? : 'valor para verdadeiro'  : 'valor para falso'
+
+const pontuacaoUSuario = 999;
+const nivelUSuario = pontuacaoUSuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal';
+console.log(nivelUSuario);
+
+//if(pontuacaoUSuario >= 1000){
+//    console.log('Usuário VIP');
+//}else{
+//    console.log('Usuário normal');
+//}aa
